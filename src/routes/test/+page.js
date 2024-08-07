88f4a4d4793ab864ff5cea52d84f96b79f0d9d35
Adam Marton <adam@adammarton.com>
2024-08-07 11:18:53 -0400

    database changes

--- src/routes/test/+page.js
+++ src/routes/test/+page.js
@@ -1,9 +1,7 @@
 //import the data from API
-export const load = async ({ fetch, params }) => {
-    //grab the vabiable passed from the url
-    const counties = params.counties; 
+export const load = async ({ fetch }) => {
     //add that variable to the API url
-    const res = await fetch(`https://schoolboard-tvt5vsz5vq-ue.a.run.app/schoolboard/candidates.json?_sort=rowid&&County__exact=${counties}`, {
+    const res = await fetch(`https://schoolboard-tvt5vsz5vq-ue.a.run.app/schoolboard/candidates.json?_sort=rowid&County__exact=Howard+County`, {
         headers: {
             "Accept": "application/json"
         }
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+
+
+  // import the data from js file api call
+  export let data; 
+  let { county } = data;
+  $: ({ county } = data); // so it stays in sync when `data` changes   
+</script>
+
+<div class="container">
+  <!--Call the componenet to show the number you can vote for
+  thisCounty is a var passed to the component and pulled from the URL / export above -->
+
+
+  <!--loop through the data and display each county where candidate answered-->
+  {#each county as thisCounty}
+    {#if  thisCounty[4] === 'y'}
+      <div class="container">
+        <p class="candidate">{thisCounty[2]}</p> 
+        <!-- pass the candidate's ID to the candidate/[id] page via the URL-->
+        <a href={`/candidates/${thisCounty[0]}`}>Read More</a>
+        <hr>
+      </div>  
+    {/if}
+  {/each}
+
+  <br><p><strong>The following candidates did not answer:</strong></p>
+  <!--loop through the data and display each county where candidate DID NOT answer-->
+  
+  {#each county as thisCounty}
+    {#if  thisCounty[4] === 'n'}
+      <div class="container">
+        <p class="candidate-no">{thisCounty[2]}</p> 
+        <hr>
+      </div>  
+    {/if}
+  {/each}
+
+</div>
