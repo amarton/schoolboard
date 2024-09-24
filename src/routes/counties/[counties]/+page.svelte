a11f4cd5d4441eb6b9baf227a630500c6c0738df
Adam Marton <adam@adammarton.com>
2024-09-24 13:20:33 -0400

    push

--- src/routes/counties/[counties]/+page.svelte
+++ src/routes/counties/[counties]/+page.svelte
@@ -7,7 +7,7 @@
   // import the data from js file api call
   export let data; 
   let { county } = data;
-  $: ({ county } = data); // so it stays in sync when `data` changes   
+  $: ({ county } = data); // so it stays in sync when `data` changes
 </script>
 
 
@@ -23,7 +23,7 @@
 
   <!--Call the componenet to show the number you can vote for
 thisCounty is a var passed to the component and pulled from the URL / export above -->
-<VoteNumber thisCounty={county[1][1]}/>
+<VoteNumber thisCounty={county[0][1]}/>
 
   <!--loop through the data and display each county where candidate answered-->
   {#each county as thisCounty}
